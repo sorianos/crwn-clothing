a9c08a5e61bffea9dbf8ae9270fa397179b9f2b5
Rafael Soriano <rafael.soriano@arteenarte.com>
2020-04-20 20:52:13 -0500

    inputs para formularios en scss y componentes

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import './sign-in.styles.scss'
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 class SignIn extends Component {
 
   constructor(props) {
@@ -33,24 +36,25 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input 
+          <FormInput 
             name='email' 
             type='email' 
             value={this.state.email}
-            onChange={this.handleChange} 
+            label='email'
+            handleChange={this.handleChange} 
             required 
           />
-          <label>Email</label>
-          <input 
+          <FormInput 
             name='password' 
             type='password' 
             value={this.state.password} 
-            onChange={this.handleChange}  
+            label='password'
+            handleChange={this.handleChange}  
             required
           />
-          <label>Password</label>
-
-          <input type='submit' value='Submit Form'/>
+          <CustomButton type='submit' >
+            Sign in
+          </CustomButton>
         </form>
       </div>
     );

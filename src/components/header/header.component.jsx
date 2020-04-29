2fba49832f3fea97930fa9316664043b281e3c8c
Rafael Soriano <rafael.soriano@arteenarte.com>
2020-04-29 13:49:43 -0500

    redux en dropdown de carrito, detalles en header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.componen';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -30,13 +32,21 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         }
+        <CartIcon />
       </div>
+      {
+        hidden ? null : <CartDropdown />
+      }
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ 
+  user: { currentUser },
+  cart: { hidden }
+}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
